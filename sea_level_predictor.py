c8d1f92d1cd04cb0b3bc37edab75cbcc8941d454
Daniel Sullivan <daniel.w.t.sullivan@gmail.com>
2024-06-05 12:53:45 +0000

    add years arrays and pass tests

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='original data') 
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level']) 
 
     # Create first line of best fit
     first_best_fit = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    
-    year_min_to_2050 = df['Year'].append(pd.Series([2050]), ignore_index=True)
+    year_min_to_2050 = np.arange(df['Year'].min(), 2051, 1)
     plt.plot(year_min_to_2050, first_best_fit.intercept + first_best_fit.slope*year_min_to_2050, label='First', color='red')
 
     # Create second line of best fit
     df_2000_beyond = df[df['Year'] >= 2000]
     second_best_fit = linregress(df_2000_beyond['Year'], df_2000_beyond['CSIRO Adjusted Sea Level'])
     
-    year_2000_to_2050 = df_2000_beyond['Year'].append(pd.Series([2050]), ignore_index=True)
+    year_2000_to_2050 = np.arange(df_2000_beyond['Year'].min(), 2051, 1)
     plt.plot(year_2000_to_2050, second_best_fit.intercept + second_best_fit.slope*year_2000_to_2050, label='Second', color='blue')
 
     # Add labels and title
